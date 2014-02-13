525397374df834e7221df8a2ae55623c4027a1e0
Bhawna <bhawna@bhawna-VirtualBox.(none)>
2014-02-13 02:10:09 -0500

    this is ubuntu app

---
+++ new/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'f79d404d4a769e2375bf9670fd2ea4d524fe908ee0fa347214d298ce64691779d1b82fa6d7793aa311e646bd68fa3987e533dfeb4eccc9b00b4b3a4771c24753';
---
+++ new/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_new_session',
+  :secret      => '17cb8ae4926ede2606406625766687884694064111e6f6eb498133549b51d330390b04e6fd67a5dfeb3a2f09148a7d34941f2f9953074bf89dd0383b2a6fd074'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
